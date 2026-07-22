16601e0a6684db7a15a9127669826e69cfd6e9bc
Saroj Dhital <208224916+Joras-Latihd@users.noreply.github.com>
2026-07-22 19:06:38 +0545

    Variables in JavaScript

---
+++ 03-Javascript-variables.js
@@ -0,0 +1,62 @@
+// String
+let firstname = "Sushant";
+let lastname = "Dhakal";
+
+// Number
+let age = 10;
+let price = 99.99;
+
+// Boolean
+let isStudent = true;
+let hasGraduated = false;
+
+// Undefined
+let message;
+
+// Null
+let middleName = null;
+
+// Array
+let subjects = ["Math", "Science", "English"];
+
+// Object
+let student = {
+  name: "Sushant Dhakal",
+  age: 10,
+  isStudent: true
+};
+
+// Constant
+const nation = "Nepal";
+
+// Symbol
+let uniqueId = Symbol("id");
+
+// BigInt
+let bigNumber = 123456789012345678901234567890n;
+
+// Printing values
+console.log("First Name:", firstname);
+console.log("Last Name:", lastname);
+console.log("Age:", age);
+console.log("Price:", price);
+console.log("Is Student:", isStudent);
+console.log("Has Graduated:", hasGraduated);
+console.log("Message (undefined):", message);
+console.log("Middle Name (null):", middleName);
+console.log("Subjects (array):", subjects);
+console.log("Student (object):", student);
+console.log("Nation (constant):", nation);
+console.log("Unique ID (symbol):", uniqueId);
+console.log("Big Number (bigint):", bigNumber);
+
+// Checking data types
+console.log(typeof firstname);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof message);
+console.log(typeof middleName);
+console.log(typeof subjects);
+console.log(typeof student);
+console.log(typeof uniqueId);
+console.log(typeof bigNumber);

a2426804296802fde8d122bf1da925babf81b545
Saroj Dhital <208224916+Joras-Latihd@users.noreply.github.com>
2026-07-26 19:18:16 +0545

    Add note: arithmetic operators

---
+++ 06-Javascript-operators.js
@@ -0,0 +1,13 @@
+/*
+1. Arithmetic Operators
+Used to perform mathematical calculations between two operands.
+*/
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder (Modulus):", a % b);
+console.log("Exponent:", a ** b);

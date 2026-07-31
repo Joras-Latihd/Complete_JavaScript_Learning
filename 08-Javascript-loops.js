fc776b4006d93241c6f7fa6297184d70a73cedce
Saroj Dhital <208224916+Joras-Latihd@users.noreply.github.com>
2026-07-31 06:49:16 +0545

    Add note: for loop

---
+++ 08-Javascript-loops.js
@@ -0,0 +1,9 @@
+/*
+1. for Loop
+Repeats a block of code a set number of times. Made of three parts:
+initialization (start), condition (when to stop), and increment
+(what happens after each pass).
+*/
+for (let i = 1; i <= 5; i++) {
+    console.log("for Loop:", i);
+}

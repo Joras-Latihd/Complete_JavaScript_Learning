a7ea5c2107b7b0d63caa93e4d253f590ddeecde8
Saroj Dhital <208224916+Joras-Latihd@users.noreply.github.com>
2026-08-03 16:06:39 +0545

    Add note: adding, updating, deleting object properties (refs #3)

--- 10-Javascript-objects.js
+++ 10-Javascript-objects.js
@@ -16,3 +16,28 @@ console.log("Bracket notation:", person["age"]);
 
 const key = "isStudent";
 console.log("Bracket notation with variable key:", person[key]);
+
+
+
+/*
+2. Adding / Updating / Deleting Properties
+Objects are mutable - properties can be added or changed anytime
+just by assigning to them. The delete keyword removes a property
+entirely from the object.
+*/
+const car = {
+    brand: "Toyota",
+    model: "Model1"
+};
+
+// Adding a new property
+car.year = 2022;
+console.log("After adding year:", car);
+
+// Updating an existing property
+car.model = "Model2";
+console.log("After updating model:", car);
+
+// Deleting a property
+delete car.year;
+console.log("After deleting year:", car);
